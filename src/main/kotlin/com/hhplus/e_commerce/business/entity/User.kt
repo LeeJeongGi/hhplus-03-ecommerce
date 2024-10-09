f876dc0dad2bc3f7e7c4c89fee6670e053e574ba
LeeJeongGi <apple4rhk@naver.com>
2024-10-10 06:21:01 +0900

    feat : H2 database 에서 User는 예약어라서 app_user로 테이블 명 변경

--- src/main/kotlin/com/hhplus/e_commerce/business/entity/User.kt
+++ src/main/kotlin/com/hhplus/e_commerce/business/entity/User.kt
@@ -1,12 +1,9 @@
 package com.hhplus.e_commerce.business.entity
 
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
+import jakarta.persistence.*
 
 @Entity
+@Table(name = "app_user")
 class User(
     @Column(name = "name")
     val name: String,
