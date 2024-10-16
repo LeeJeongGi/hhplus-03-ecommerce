5a88af41e232b12ef9ca7c8e147e99a9e5562432
LeeJeongGi <apple4rhk@naver.com>
2024-10-16 22:18:07 +0900

    feat : 주문, 결제 Facade Class 추가

---
+++ src/main/kotlin/com/hhplus/e_commerce/business/facade/OrderFacade.kt
@@ -0,0 +1,39 @@
+package com.hhplus.e_commerce.business.facade
+
+import com.hhplus.e_commerce.business.dto.OrderSaveDto
+import com.hhplus.e_commerce.business.facade.dto.OrderSaveResultDto
+import com.hhplus.e_commerce.business.service.BalanceService
+import com.hhplus.e_commerce.business.service.OrderService
+import com.hhplus.e_commerce.business.service.ProductStockService
+import com.hhplus.e_commerce.common.error.code.ErrorCode
+import com.hhplus.e_commerce.common.error.exception.BusinessException
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class OrderFacade(
+    private val productStockService: ProductStockService,
+    private val balanceService: BalanceService,
+    private val orderService: OrderService,
+) {
+
+    @Transactional
+    fun saveOrder(orderSaveDto: OrderSaveDto): OrderSaveResultDto {
+        // 상품 존재 하는지 조회
+        val orderProducts = productStockService.valid(orderSaveDto.products)
+
+        // 유저 조회 - 유저 잔액 검증
+        val userBalance = balanceService.getUserBalance(orderSaveDto.userId)
+        if (userBalance.currentAmount < orderSaveDto.totalAmount) {
+            BusinessException.BadRequest(ErrorCode.User.INSUFFICIENT_BALANCE)
+        }
+
+        // 주문 정보 저장
+        val result = orderService.save(orderSaveDto)
+
+        // 재고 정보 업데이트
+        productStockService.updateStock(orderSaveDto.products)
+
+        return result
+    }
+}
---
+++ src/main/kotlin/com/hhplus/e_commerce/business/facade/PaymentFacade.kt
@@ -0,0 +1,54 @@
+package com.hhplus.e_commerce.business.facade
+
+import com.hhplus.e_commerce.business.dto.PaymentSaveDto
+import com.hhplus.e_commerce.business.dto.PaymentSaveResultDto
+import com.hhplus.e_commerce.business.service.BalanceService
+import com.hhplus.e_commerce.business.service.OrderService
+import com.hhplus.e_commerce.business.service.PaymentService
+import com.hhplus.e_commerce.common.error.code.ErrorCode
+import com.hhplus.e_commerce.common.error.exception.BusinessException
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class PaymentFacade(
+    private val orderService: OrderService,
+    private val balanceService: BalanceService,
+    private val paymentService: PaymentService,
+) {
+
+    @Transactional
+    fun processPayment(orderId: Long): PaymentSaveResultDto {
+
+        // 1. 주문 정보 조회
+        val order = orderService.findOrderById(orderId)
+
+        // 2. 주문이 유효한지 확인
+        if (order.status != "ORDERED") {
+            throw BusinessException.BadRequest(ErrorCode.Order.INVALID_ORDER_STATUS)
+        }
+
+        // 3. 유저 잔액 확인
+        val userBalance = balanceService.getUserBalance(order.userId)
+        if (userBalance.currentAmount < order.totalAmount) {
+            throw BusinessException.BadRequest(ErrorCode.User.INSUFFICIENT_BALANCE)
+        }
+
+        // 4. 잔액 차감
+        val paymentUserBalance = balanceService.changeBalance(order.userId, order.totalAmount)
+
+        val paymentSaveDto = PaymentSaveDto(
+            userId = order.userId,
+            orderId = order.orderId,
+            status = "COMPLETED",
+            amount = order.totalAmount,
+        )
+        // 5. 결제 정보 저장
+        val result = paymentService.save(paymentSaveDto)
+
+        // 6. 주문 상태 업데이트
+        orderService.updateOrderStatus(order.orderId, "PAID")
+
+        return result
+    }
+}
