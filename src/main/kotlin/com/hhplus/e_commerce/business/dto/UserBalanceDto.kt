6f7619bd2eba25f03ea1e9ea4c47fdc7797dcc0e
LeeJeongGi <apple4rhk@naver.com>
2024-11-06 06:42:03 +0900

    feat : 유저 잔액 조회 할 때 캐시 적용

--- src/main/kotlin/com/hhplus/e_commerce/business/dto/UserBalanceDto.kt
+++ src/main/kotlin/com/hhplus/e_commerce/business/dto/UserBalanceDto.kt
@@ -3,12 +3,13 @@ package com.hhplus.e_commerce.business.dto
 import com.hhplus.e_commerce.business.entity.Balance
 import com.hhplus.e_commerce.common.error.code.ErrorCode
 import com.hhplus.e_commerce.common.error.exception.BusinessException
+import java.io.Serializable
 
 data class UserBalanceDto(
     val balanceId: Long,
     val userId: Long,
     val currentAmount: Int,
-) {
+) : Serializable {
     fun isEnoughMoney(orderAmount: Int) {
         if (this.currentAmount < orderAmount) {
             throw BusinessException.BadRequest(ErrorCode.User.INSUFFICIENT_BALANCE)
