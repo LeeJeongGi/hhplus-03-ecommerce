af04e3aef29b5674d983b7786bf06e7c076be451
LeeJeongGi <apple4rhk@naver.com>
2024-11-20 22:34:46 +0900

    feat : docker-compose로 mysql, kafka 설치

---
+++ src/main/kotlin/com/hhplus/e_commerce/common/config/KafkaConfig.kt
@@ -0,0 +1,54 @@
+package com.hhplus.e_commerce.common.config
+
+import org.apache.kafka.clients.admin.NewTopic
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.kafka.common.serialization.StringSerializer
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.annotation.EnableKafka
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
+import org.springframework.kafka.core.*
+
+
+@EnableKafka
+@Configuration
+class KafkaConfig {
+
+    @Bean
+    fun producerFactory(): ProducerFactory<String, String> {
+        val config: MutableMap<String, Any> = HashMap()
+        config[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = "127.0.0.1:9092"
+        config[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
+        config[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
+        return DefaultKafkaProducerFactory(config)
+    }
+
+    @Bean
+    fun kafkaTemplate(): KafkaTemplate<String, String> {
+        return KafkaTemplate(producerFactory())
+    }
+
+    @Bean
+    fun consumerFactory(): ConsumerFactory<String, String> {
+        val config: MutableMap<String, Any> = HashMap()
+        config[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = "127.0.0.1:9092"
+        config[ConsumerConfig.GROUP_ID_CONFIG] = "example-group"
+        config[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
+        config[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
+        return DefaultKafkaConsumerFactory(config)
+    }
+
+    @Bean
+    fun kafkaListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, String> {
+        val factory = ConcurrentKafkaListenerContainerFactory<String, String>()
+        factory.consumerFactory = consumerFactory()
+        return factory
+    }
+
+    @Bean
+    fun createTopic(): NewTopic {
+        return NewTopic("ecommerce27", 1, 1.toShort())
+    }
+}
