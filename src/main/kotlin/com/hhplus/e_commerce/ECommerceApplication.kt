7138f8d8328f3620dc3822b6f2c5528ee9b5ef2f
LeeJeongGi <apple4rhk@naver.com>
2024-11-21 23:13:38 +0900

    feat : 카프카 컨슈머 기능 추가

--- src/main/kotlin/com/hhplus/e_commerce/ECommerceApplication.kt
+++ src/main/kotlin/com/hhplus/e_commerce/ECommerceApplication.kt
@@ -4,12 +4,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.kafka.annotation.EnableKafka
 import org.springframework.scheduling.annotation.EnableAsync
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableCaching
 @EnableAsync
+@EnableKafka
 class ECommerceApplication
 
 fun main(args: Array<String>) {
