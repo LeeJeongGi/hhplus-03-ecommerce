737f860f058af6c5f288c0c5dcc2427353af88b4
LeeJeongGi <apple4rhk@naver.com>
2024-11-27 22:14:26 +0900

    refactor : Dto Class 직렬화

--- src/main/kotlin/com/hhplus/e_commerce/business/dto/ProductMetaDto.kt
+++ src/main/kotlin/com/hhplus/e_commerce/business/dto/ProductMetaDto.kt
@@ -1,12 +1,13 @@
 package com.hhplus.e_commerce.business.dto
 
 import com.hhplus.e_commerce.business.entity.Product
+import java.io.Serializable
 
 data class ProductMetaDto(
     val productId: Long,
     val name: String,
     val price: Int,
-) {
+) : Serializable {
     companion object {
         fun from(product: Product): ProductMetaDto {
             return ProductMetaDto(
